a7a4ce62fd717ffd86a7cb5a46c60d52caa283e2
ibra360 <a.ebrahim360@outlook.com>
2022-01-15 14:18:02 +0500

    Completed task 5 and 6

---
+++ Tasks/task5.js
@@ -0,0 +1,21 @@
+import Web3 from "web3";
+import ERC20TransferABI from "../abi.js";
+
+
+const ETH_NODE_URL =
+  "https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213";
+let web3 = new Web3(new Web3.providers.HttpProvider(ETH_NODE_URL));
+const txnHash="0x2f6189726e3d5e3b1e56f2dbca7f1ae60d21dc29e91a7fbb8004c8dbf1fc8587"
+
+web3.eth.getTransactionReceipt(txnHash, function (e, data) {
+    if (e !== null) {
+        console.log("Could not find a transaction for your id! ID you provided was " + txnHash);
+    } else {
+        console.log(data);
+        if(data.status == '0x0') {
+            console.log("The contract execution was not successful, check your transaction !");
+        } else {
+            console.log("Execution worked fine! And the gas used in this txn is :",data.gasUsed );
+        }
+    }
+})
---
+++ Tasks/task6.js
@@ -0,0 +1,24 @@
+
+
+
+import Web3 from "web3";
+import ERC20TransferABI from "../abi.js";
+
+
+const ETH_NODE_URL =
+  "https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213";
+let web3 = new Web3(new Web3.providers.HttpProvider(ETH_NODE_URL));
+const txnHash="0xf83387377922b83bebdfebcde8b89e69daf91cdc44d5e2fa919b5db2b66e55b0"
+
+web3.eth.getTransaction(txnHash, function (e, data) {
+    if (e !== null) {
+        console.log("Could not find a transaction for your id! ID you provided was " + txnHash);
+    } else {
+        console.log(data);
+        if(data.status == '0x0') {
+            console.log("The contract execution was failed!");
+        } else {
+            console.log(`Execution worked fine! \n Sender :${data.from} \n Reciever ${data.to} \n Number of tokens in wei: ${data.value} `);
+        }
+    }
+})
